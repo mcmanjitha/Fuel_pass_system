8f3197a30a0b6ebe56a29bd2026f990250ef5507
supun-ganegoda <supung@syntaxgenie.com>
2024-10-24 20:15:36 +0530

    Fuel station registration form added

--- web-app/src/screens/tabbed-layout.tsx
+++ web-app/src/screens/tabbed-layout.tsx
@@ -1,16 +1,21 @@
 import { Tab, Tabs } from "react-bootstrap";
 import RegisterScreen from "./register-screen";
 import LoginScreen from "./login-screen";
+import FuelStationRegisterScreen from "./fuel-station-registration";
 
 const TabbedLayout = () => {
   return (
     <Tabs defaultActiveKey="register" id="tabbed-layout" fill>
-      <Tab eventKey="register" title="Register Form">
+      <Tab eventKey="register" title="User Registration">
         <RegisterScreen />
       </Tab>
 
-      <Tab eventKey="login" title="Login Form">
+      <Tab eventKey="login" title="User Login">
         <LoginScreen />
+      </Tab>
+
+      <Tab eventKey="fuel-register" title="Fuel Station Registration">
+        <FuelStationRegisterScreen />
       </Tab>
 
       <Tab eventKey="administration" title="Administration" disabled>
